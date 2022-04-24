c8aa9ba45cdc374672f1dd7628905612498c974d
Reinhardt <reinhardt.cgr@gmail.com>
2022-04-24 19:56:25 +1200

    added a loop to get user input

--- main.rb
+++ main.rb
@@ -1,1 +1,28 @@
-puts 'testing...'
+def main
+  @prompt = 'Circadian interface online:...'
+  puts @prompt
+
+  $stdout.write('$ ...')
+
+  get_user_input = true
+  while (get_user_input)
+    user_input = gets.chomp
+    $stdout.flush
+    puts '\rthe user input is: ' + user_input
+
+    if user_input == 'start'
+      puts '5:00'
+    end
+
+    # how can I replace the top line
+    # then have the second line as the user input just in case
+
+    if user_input == 'exit'
+      puts 'Goodbye :)'
+      get_user_input = false
+    end
+  end
+
+end
+
+main
