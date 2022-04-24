cf408287ab3438f8d4149f937448bf251c01223b
Reinhardt <reinhardt.cgr@gmail.com>
2022-04-24 21:07:36 +1200

    moved things into classes to have logic a little more compartmentalized

--- main.rb
+++ main.rb
@@ -1,36 +1,63 @@
-def main
-  #@prompt = 'Circadian interface online:...'
-  #puts @prompt
+class Task
+  attr_accessor :hours, :minutes, :seconds
 
-  #sys.stdout.write('\r$ ...')
+  def initialize(hours, minutes, seconds)
+    @hours = hours
+    @minutes = minutes
+    @seconds = seconds
+  end
+  
+  def add_padding(time)
+    if time < 10
+      "0#{time}"
+    else
+      time
+    end
+  end
 
-  puts 'testing ...'
-  sleep 1
-  $stdout.print("\033[1A")
-  puts 'does this work???'
-  #$stdout.print("one more time. ...")
-  #$stdout.flush
-
-  #$stdout.puts($stdin.gets.inspect)
-
-  #get_user_input = true
-  #while (get_user_input)
-    #user_input = gets.chomp
-    #sys.stdout.flush()
-    #sys.stdout.write "\rthe user input is: #{user_input}" 
-    #if user_input == 'start'
-      #puts '5:00'
-    #end
-
-     #how can I replace the top line
-     #then have the second line as the user input just in case
-
-    #if user_input == 'exit'
-      #puts 'Goodbye :)'
-      #get_user_input = false
-    #end
-  #end
-
+  def to_s
+    "#{add_padding(@hours)}:#{add_padding(@minutes)}:#{add_padding(@seconds)}"
+  end
 end
 
-main
+class Circadian
+  def initialize
+    @prompt = 'Circadian interface online...'
+    @task = Task.new(0, 1, 29)
+  end
+
+  def run
+    puts @prompt
+
+    puts '00:00:00> testing ...'
+    sleep 1
+    $stdout.print("\033[1A")
+    puts "#{@task}> does this work???"
+    #$stdout.print("one more time. ...")
+    #$stdout.flush
+
+    #$stdout.puts($stdin.gets.inspect)
+
+    #get_user_input = true
+    #while (get_user_input)
+      #user_input = gets.chomp
+      #sys.stdout.flush()
+      #sys.stdout.write "\rthe user input is: #{user_input}" 
+      #if user_input == 'start'
+        #puts '5:00'
+      #end
+
+       #how can I replace the top line
+       #then have the second line as the user input just in case
+
+      #if user_input == 'exit'
+        #puts 'Goodbye :)'
+        #get_user_input = false
+      #end
+    #end
+
+  end
+end
+
+circadian = Circadian.new
+circadian.run
