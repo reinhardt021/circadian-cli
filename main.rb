8613a48cf36d5422fb517d818fbd0517722b9e9b
Reinhardt <reinhardt.cgr@gmail.com>
2022-04-24 21:59:43 +1200

    have the outputs showing

--- main.rb
+++ main.rb
@@ -8,11 +8,7 @@ class Task
   end
   
   def add_padding(time)
-    if time < 10
-      "0#{time}"
-    else
-      time
-    end
+    time < 10 ? "0#{time}" : time 
   end
 
   def to_s
@@ -23,38 +19,47 @@ end
 class Circadian
   def initialize
     @prompt = 'Circadian interface online...'
-    @task = Task.new(0, 1, 29)
+    @task = Task.new(0, 1, 0)
+  end
+
+  def log(task, message)
+    #puts '00:00:00> '
+    $stdout.print("\033[1A")
+    #$stdout.print("\033[2J") # clears the whole thing
+    #$stdout.print("\033[K")
+    $stdout.print("\033[2K")
+    $stdout.puts("#{task}> #{message}")
+    #puts "#{task}> #{message}"
   end
 
   def run
     puts @prompt
 
-    puts '00:00:00> testing ...'
+    #puts "#{@task}> Let's get started"
+    log(@task, "Let's get started")
     sleep 1
-    $stdout.print("\033[1A")
-    puts "#{@task}> does this work???"
-    #$stdout.print("one more time. ...")
-    #$stdout.flush
+    #print "#{@task}> "
+    log(@task, "")
 
-    #$stdout.puts($stdin.gets.inspect)
-
-    #get_user_input = true
-    #while (get_user_input)
-      #user_input = gets.chomp
-      #sys.stdout.flush()
+    get_user_input = true
+    while (get_user_input)
+      #user_input = "somethign"
+      user_input = gets.chomp
       #sys.stdout.write "\rthe user input is: #{user_input}" 
-      #if user_input == 'start'
+      if user_input == 'start'
         #puts '5:00'
-      #end
+        log(@task, "starting now ... ")
+        # start countdown sequence and output
+      end
 
        #how can I replace the top line
        #then have the second line as the user input just in case
 
-      #if user_input == 'exit'
-        #puts 'Goodbye :)'
-        #get_user_input = false
-      #end
-    #end
+      if user_input == 'exit'
+        puts 'Goodbye :)'
+        get_user_input = false
+      end
+    end
 
   end
 end
